c66c6483affe147c6b5357d1e1c7a0b76880d935
Cia <dragaoking123@gmail.com>
2026-01-18 10:01:56 -0400

    feat: add Tauri CLI with native bindings and command execution
    - Introduced new files for Tauri CLI including index.js, main.js, and tauri.js.
    - Added TypeScript definitions for the CLI in index.d.ts and main.d.ts.
    - Implemented error logging and command execution functionality.
    - Created package.json for Tauri CLI with necessary dependencies and scripts.
    - Added package-lock.json to lock dependencies.
    - Implemented a basic header with login functionality in header.js and header.html.

---
+++ js/header.js
@@ -0,0 +1,7 @@
+const botaoLogin = container.querySelector("button");
+const botaoCadastro = container.querySelector("button");
+const botaoToDo = container.querySelector('button');
+
+    botaoEntrar.addEventListener('click',() =>{
+    window.location.href = '/pages/login';
+    });
